cd191f0344d7a4ce6a632f0f7ef92d2ad75c9f3e
Collinsoden22 <Collinsoden22@gmail.com>
2022-04-08 10:24:07 -0700

    Added task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*This will prints 'Programing is like building a multilingual puzzle
+*Return 0 if the code complies correctly, else throw error
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
+}
