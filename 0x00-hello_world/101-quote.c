c0dd1143ab8d34a2a32094b1bc254ceb8a5e9332
Collinsoden22 <Collinsoden22@gmail.com>
2022-04-08 12:48:53 -0700

    Last task to print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main- This will print the message 'and that piece of art is useful'
+ * - Dora Korpar, 2005-10-19'
+ *   followed by a new line, to the standard error
+ * Return: This will return 0 in this case
+**/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	return (1);
+}
