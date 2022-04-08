b9b7b52c96b498d5e98012b3ec321674141a1b2c
Collinsoden22 <Collinsoden22@gmail.com>
2022-04-08 10:36:31 -0700

    This program prints something to the screen using printf statement

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/**
+ * main- This prints 'with proper grammar, but the outcome is a piece of art,
+ * Return: This returns 0 if the code runs correctly or 1 if it doesn't
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
