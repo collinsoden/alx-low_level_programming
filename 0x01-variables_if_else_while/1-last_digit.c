c9712e75085a56f350c781d27aa39b582a680330
Collinsoden22 <Collinsoden22@gmail.com>
2022-04-09 10:35:20 -0700

    Task One: a New value is assigned to one, run and prints its result

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * main- This will assign a number to the variable n
+ * each time the code is executed
+ * Return: Will return 0 is the code runs without errors
+ **/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d", n, n % 10);
+	if (n % 10 > 5)
+	{
+		printf("and is greater tha 5\n");
+	} else if (n % 10 == 0)
+	{
+		printf("and is 0\n");
+	} else if (n % 10 < 6 && n % 10 != 0)
+	{
+		printf("and is less than 6 and not zero\n");
+	}
+	return (0);
+}
