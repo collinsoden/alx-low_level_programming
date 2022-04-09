2f0eb27c1541f85df5f86320662676bd16ab0007
Collinsoden22 <Collinsoden22@gmail.com>
2022-04-09 10:52:10 -0700

    This will print alphabets from a to z in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this will print alphabets from a to z in lowercase
+ * Return: Always (0) Success
+ **/
+int main(void)
+{
+	putchar("{a..z} \n");
+	return (0);
+}
