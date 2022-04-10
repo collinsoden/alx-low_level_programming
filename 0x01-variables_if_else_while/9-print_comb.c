6acea0f24734df77c077323d44ee69c9fb80a5cd
Collinsoden22 <Collinsoden22@gmail.com>
2022-04-10 04:50:32 -0700

    print all possible combinations of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <sdtio.h>
+/**
+ * main - prints all possible combinations of single digit numbers
+ * Return: always (0) success
+ **/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+		putchar(', ');
+		n++;
+	}
+		return (0);
+}
