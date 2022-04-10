f2bed2e7488e0a3fc017f5f0370fc0f2d17b5853
Collinsoden22 <Collinsoden22@gmail.com>
2022-04-10 00:40:42 -0700

    Print all letters of the alphabet in reverse"

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all letters of the alphabet in reverse
+ * Return: Always (0) success
+ **/
+int main(void)
+{
+	char l;
+
+	for(l= 'z'; l >= 'a'; l--)
+	{
+		putchar(n);
+	}
+		putchar('\n');
+		return (0);
+}
