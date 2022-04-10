bafc0d9dee11f1cb2a8124cf9b8ac546d9619af1
Collinsoden22 <Collinsoden22@gmail.com>
2022-04-10 00:07:06 -0700

    Added task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Will print all single digits starting form base 0
+ * Return: Always (0) Sucess
+ **/
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n < '10'; n++)
+	{	
+		putchar('n');
+	}
+		putchar('\n');
+		return (0);
+	
+}
