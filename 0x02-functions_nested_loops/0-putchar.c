1b98f0f74f89719bae1e65e26adb9bb406dee071
Collinsoden22 <Collinsoden22@gmail.com>
2022-04-12 08:54:45 -0700

    Print putchar to this file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this will print _putchar
+ * Return: Always (0) Success
+ **/
+int main(void)
+{
+	char ch = "_putchar";
+
+	printf(ch);
+	pintf('\n');
+	return (0);
+}
